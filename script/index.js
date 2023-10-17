96b9c0dc8804dd37476e114d4b4d07558bd1da26
Alex Fang <106351391+alexfang324@users.noreply.github.com>
2023-10-17 11:16:07 -0700

    initial commit

---
+++ script/index.js
@@ -0,0 +1,16 @@
+/* basic cross-page javascript */
+
+
+/* navigation bar javascript below */
+
+
+/* index page javascript below */
+
+
+/* about page javascript below */
+
+
+/* our-team page javascript below */
+
+
+/* login form javascript below */
